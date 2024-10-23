c9c430b6f9b7e86fe447466aaf6a5c35612622db
Moses Minja <minjastermoses@gmail.com>
2024-10-23 12:27:55 +0300

    Minor changes

--- client/lib/build-client.ts
+++ client/lib/build-client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default (headers?) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'https://metabloit.xyz',
+      baseURL: 'http://metabloit.xyz',
       headers,
     });
   } else {
