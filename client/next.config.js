a6521de1d28a6fac7c5aa0dab55720fe6fed7a98
Moses Minja <minjastermoses@gmail.com>
2024-10-25 04:30:07 +0300

    Minor fixes

--- client/next.config.js
+++ client/next.config.js
@@ -8,10 +8,4 @@ module.exports = {
     return config;
   },
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_API_URL:
-      process.env.NODE_ENV === 'production'
-        ? 'https://metabloit.xyz'
-        : 'http://localhost:3000',
-  },
 };
